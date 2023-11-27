5c884868c78b4c7cd0d15c78ebb6f6ef1fbeba81
Akshay-Luhaidya-1999 <akshayluhadiya9@gmail.com>
2023-11-27 18:22:16 +0530

    Update EmployeServiceApplication.java
    Add Sysout statement for welcome

--- Employe-Service/src/main/java/com/employee/service/EmployeServiceApplication.java
+++ Employe-Service/src/main/java/com/employee/service/EmployeServiceApplication.java
@@ -10,6 +10,7 @@ public class EmployeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeServiceApplication.class, args);
+		System.out.println("Welcome");
 	}
 
 }
